e621eae2bfee68ca82b4e5702bcaa2eab0b0c6ce
clone <bjbs08@qq.com>
2020-11-06 08:57:42 +0800

    添加文件上传与多文件上传

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/gin-gonic/gin"
 )
@@ -33,18 +35,19 @@ func main() {
 			"title": "Users website",
 		})
 	})
+	// 模板函数 函数需要写在解析模板前
 	r.GET("/user/user_func", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "user_func.tmpl", gin.H{
 			"title": `<a href='http://www.baidu.com' target='_blank'>百度</a>`,
 		})
 	})
-
+	//
 	r.GET("/posts", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Posts website",
 		})
 	})
-
+	// 测试模板
 	r.GET("/home/index.html", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
@@ -57,7 +60,7 @@ func main() {
 	r.GET("/home/services.html", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "services.html", nil)
 	})
-
+	// Query 参数获取
 	r.GET("/query", func(c *gin.Context) {
 		name := c.Query("name")
 		age := c.DefaultQuery("age", "18")
@@ -66,11 +69,11 @@ func main() {
 			"age":  age,
 		})
 	})
-
+	// 表单提交 GET
 	r.GET("/post_form", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "post_form.tmpl", nil)
 	})
-
+	// 表单提交 POST
 	r.POST("/post_form", func(c *gin.Context) {
 		username := c.PostForm("username")
 		password := c.PostForm("password")
@@ -81,7 +84,7 @@ func main() {
 			"data": username + "----" + password,
 		})
 	})
-
+	// URI
 	r.GET("/uri/:username/:age", func(c *gin.Context) {
 		username := c.Param("username")
 		age := c.Param("age")
@@ -90,7 +93,7 @@ func main() {
 			"age":      age,
 		})
 	})
-
+	// 参数绑定
 	r.GET("/should_bind", func(c *gin.Context) {
 		var loginForm LoginForm
 		err := c.ShouldBind(&loginForm) //支持post get
@@ -104,6 +107,58 @@ func main() {
 				"password": loginForm.Password,
 			})
 		}
+
+	})
+	// 文件上传 GET
+	r.GET("/file_upload", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "file_upload.tmpl", nil)
+	})
+	// 文件上传 POST
+	r.POST("/file_upload", func(c *gin.Context) {
+		f, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			dir := "./uploads/files/"
+			// 判断上传目录是否存在 如果不存在则创建文件夹
+			_, err := os.Stat(dir)
+			if os.IsNotExist(err) {
+				// 递归创建  os.Mkdir 创建单个
+				os.MkdirAll(dir, os.ModePerm)
+			}
+
+			distDir := path.Join(dir, f.Filename)
+			c.SaveUploadedFile(f, distDir)
+			c.JSON(http.StatusOK, gin.H{
+				"status": "ok",
+			})
+		}
+	})
+
+	// 多文件上传 GET
+	r.GET("/file_muti", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "file_muti.tmpl", nil)
+	})
+	// 多文件上传 POST
+	r.POST("/file_muti", func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+		dir := "./uploads/files/"
+		// 判断上传目录是否存在 如果不存在则创建文件夹
+		_, err := os.Stat(dir)
+		if os.IsNotExist(err) {
+			// 递归创建  os.Mkdir 创建单个
+			os.MkdirAll(dir, os.ModePerm)
+		}
+		for _, file := range files {
+			distDir := path.Join(dir, file.Filename)
+			c.SaveUploadedFile(file, distDir)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
 
 	})
 
